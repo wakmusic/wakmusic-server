72b5fcd13be273d95848e1d753944574e3aa1160
JWP <98960141+qkrwldnjs@users.noreply.github.com>
2022-10-18 17:47:41 +0900

    Required module for routes/charts.js

---
+++ modules/SQL.js
@@ -0,0 +1,36 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./src/database/charts.db');
+
+class SQL {
+    all(query) {
+        return new Promise(function (resolve, reject) {
+            db.all(query, (err, rows) => {
+                if (err) reject(err);
+                else resolve(rows);
+            });
+        });
+    }
+
+    run(query) {
+        return new Promise(function (resolve, reject) {
+            db.run(query, (err) => {
+                if (err) reject(err);
+            });
+        });
+    }
+
+    get(query, params) {
+        return new Promise(function (resolve, reject) {
+            db.get(query, params, (err, row) => {
+                if (err) reject(err);
+                else resolve(row);
+            });
+        });
+    }
+
+    escape(string) {
+        return db.escape(string);
+    }
+}
+
+module.exports = SQL;
